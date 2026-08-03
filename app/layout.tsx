0659cefbbb5a8cb78028f831a6a3349dd16d370d
Sadat Shahriar Rafin <sadatshahriarrafin@gmail.com>
2026-08-03 23:04:55 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,16 +15,16 @@ const schibstedGrotesk = Schibsted_Grotesk({
 const martianMono = Martian_Mono({
   variable: "--font-martian-mono",
   subsets: ["latin"],
-});
+});  
 
 export const metadata: Metadata = {
   title: "DevEvent",
   description: "The Hub for Every Dev Event You Mustn't Miss",
-};
+};  
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: Readonly<{  
   children: React.ReactNode;
 }>) {
   return (
